52d869a835b6d147275a96e53a705f912c889067
Ben <benjaminrsteele@gmail.com>
2024-03-12 16:43:06 -0500

    waiting for robot:(

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -9,6 +9,7 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -17,6 +18,7 @@ public class Intake extends SubsystemBase {
   
   CANSparkMax intakeMotor;
   DigitalInput shooterLimitSwitch;
+  DigitalOutput led0;
   
   /** Creates a new Intake. */
   public Intake() {
@@ -26,6 +28,8 @@ public class Intake extends SubsystemBase {
 
   shooterLimitSwitch = new DigitalInput(0);
 
+  //led0 = new DigitalOutput(0);
+
   
   }
 
@@ -33,9 +37,11 @@ public class Intake extends SubsystemBase {
   public void intakeSpin(double speed) {
     if (shooterLimitSwitch.get() == false) {
       intakeMotor.set(0);
+      //led0.set(true);
     }
     else {
     intakeMotor.set(speed);
+      //led0.set(false);
     }
   }
 
